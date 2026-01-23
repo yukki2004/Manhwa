5eaf743bd981873b140e0a05de0ce3e476d10f6d
yukki2004 <dangsykien09@gmail.com>
2026-01-24 00:13:21 +0700

    feat(notification) thêm các chức năng mapping cho thông báo

---
+++ Src/Manhwa.Application/Common/Abstractions/NotificationResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manhwa.Application.Common.Abstractions
+{
+    public class NotificationResult
+    {
+        public long NotificationId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string? RedirectUrl { get; set; } 
+        public string? Metadata { get; set; } 
+        public DateTimeOffset CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+
+        // Thông tin hiển thị "Vỏ bọc"
+        public string SenderName { get; set; } = "Hệ thống TruyenVerse";
+        public string SenderAvatar { get; set; } = "/assets/images/system-avatar.png";
+    }
+}
---
+++ Src/Manhwa.Application/Common/Mappings/NotificationMapping.cs
@@ -0,0 +1,42 @@
+﻿using Manhwa.Application.Common.Abstractions;
+using Manhwa.Application.Common.Extensions;
+using Manhwa.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manhwa.Application.Common.Mappings
+{
+    public static class NotificationMapping
+    {
+        public static NotificationResult MapToResult(Notification notification, bool isRead)
+        {
+            var result = new NotificationResult
+            {
+                NotificationId = notification.NotificationId,
+                Title = notification.Title,
+                Content = notification.Content,
+                Type = notification.Type.ToString(),
+                RedirectUrl = notification.RedirectUrl,
+                Metadata = notification.Metadata,
+                CreatedAt = notification.CreatedAt,
+                IsRead = isRead
+            };
+
+            if (notification.Sender != null)
+            {
+                result.SenderName = notification.Sender.Username;
+                result.SenderAvatar = notification.Sender.Avatar.ToFullUrl();
+            }
+            else
+            {
+                result.SenderName = "Hệ thống TruyenVerse";
+                result.SenderAvatar.ToFullUrl();
+            }
+
+            return result;
+        }
+    }
+}
