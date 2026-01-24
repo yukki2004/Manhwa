9dfd77744a7759f5bf22717c33d0871bac8a0d2b
yukki2004 <dangsykien09@gmail.com>
2026-01-24 13:43:47 +0700

    feat(exception) thêm exception toàn cục

---
+++ Src/Manhwa.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manhwa.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Thực thể \"{name}\" có ID ({key}) không tồn tại.") { }
+    }
+}
---
+++ Src/Manhwa.Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Manhwa.Application.Common.Exceptions;
+using Manhwa.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Manhwa.Infrastructure.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlingMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try { await _next(context); }
+            catch (Exception ex) { await HandleExceptionAsync(context, ex); }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = exception switch
+            {
+                NotFoundException => HttpStatusCode.NotFound,
+                ForbiddenAccessException => HttpStatusCode.Forbidden,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(new
+            {
+                error = exception.Message,
+                status = (int)code,
+                timestamp = DateTime.UtcNow
+            }));
+        }
+    }
+}
