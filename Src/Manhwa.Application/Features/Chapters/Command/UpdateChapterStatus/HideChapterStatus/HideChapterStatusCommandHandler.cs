3aa48dc07a8da903a8a0beb3a51b994b707263b6
yukki2004 <dangsykien09@gmail.com>
2026-01-27 21:33:52 +0700

    feat(chapter) thêm chức năng hide chapter

--- Src/Manhwa.Application/Features/Chapters/Command/UpdateChapterStatus/HideChapterStatus/HideChapterStatusCommandHandler.cs
+++ Src/Manhwa.Application/Features/Chapters/Command/UpdateChapterStatus/HideChapterStatus/HideChapterStatusCommandHandler.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Amazon.Runtime.Internal;
+using Manhwa.Application.Common.Exceptions;
+using Manhwa.Application.Common.Interfaces;
+using Manhwa.Application.Features.Chapters.Command.UpdateChapterStatus.PublishChapterStatus;
+using Manhwa.Domain.Exceptions;
+using Manhwa.Domain.Repositories;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,69 @@ using System.Threading.Tasks;
 
 namespace Manhwa.Application.Features.Chapters.Command.UpdateChapterStatus.HideChapterStatus
 {
-    class HideChapterStatusCommandHandler
+    public class HideChapterStatusCommandHandler : IRequestHandler<HideChapterStatusCommand, HideChapterStatusResponse>
     {
+        private readonly IChapterRepository _chapterRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public HideChapterStatusCommandHandler(IChapterRepository chapterRepository, IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _chapterRepository = chapterRepository;
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<HideChapterStatusResponse> Handle(HideChapterStatusCommand commad, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(commad.UserId);
+            if (user == null)
+            {
+                throw new NotFoundException("user", commad.UserId);
+            }
+            if (user.IsActive == false)
+            {
+                throw new ForbiddenAccessException();
+            }
+            var chapter = await _chapterRepository.GetWithStoryByIdAsync(commad.ChapterId);
+            if (chapter == null)
+            {
+                throw new NotFoundException("chapter", commad.ChapterId);
+            }
+            if (chapter.Story == null)
+            {
+                throw new NotFoundException("story", chapter.StoryId);
+            }
+            if (chapter.Story.UserId != commad.UserId && commad.UserRole != "Admin")
+            {
+                throw new ForbiddenAccessException();
+            }
+            if (chapter.Story.AdminLockStatus == Domain.Enums.AdminLockStatus.Locked && commad.UserRole != "Admin")
+            {
+                throw new BusinessRuleViolationException(
+                    $"Không thể thao tác: Truyện đã bị khóa bởi Admin. Lý do: {chapter.Story.AdminNote}",
+                    "STORY_IS_LOCKED");
+            }
+            if (chapter.Story.IsPublish == Domain.Enums.Story.StoryPublishStatus.Deleted && commad.UserRole != "Admin")
+            {
+                throw new BusinessRuleViolationException("Không thể thao tác trên chương của truyện đã bị xóa", "STORY_DELETED");
+            }
+            if (chapter.Status == Domain.Enums.Chapter.ChapterStatus.Deleted && commad.UserRole != "Admin")
+            {
+                throw new BusinessRuleViolationException("Không thể thao tác trên chương đã bị xóa", "CHAPTER_DELETED");
+            }
+            if (chapter.Status == Domain.Enums.Chapter.ChapterStatus.Hidden)
+            {
+                throw new BusinessRuleViolationException("Chương đã bị ẩn", "CHAPTER_ALREADY_HIDDEN");
+            }
+            chapter.Status = Domain.Enums.Chapter.ChapterStatus.Hidden;
+            chapter.UpdatedAt = DateTimeOffset.UtcNow;
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return new HideChapterStatusResponse
+            {
+                ChapterId = chapter.ChapterId,
+                Status = chapter.Status.ToString(),
+                Message = "Chapter status hiden successfully.",
+                UpdatedAt = chapter.UpdatedAt
+            };
+        }
     }
 }
--- Src/Manhwa.WebAPI/Controllers/Chapters/ChapterController.cs
+++ Src/Manhwa.WebAPI/Controllers/Chapters/ChapterController.cs
@@ -5,6 +5,7 @@ using Manhwa.Application.Features.Chapters.Command.AddChapter;
 using Manhwa.WebAPI.Extensions;
 using Manhwa.Application.Features.Chapters.Command.UpdateChapterStatus.DeleteChapterStatus;
 using Manhwa.Application.Features.Chapters.Command.UpdateChapterStatus.PublishChapterStatus;
+using Manhwa.Application.Features.Chapters.Command.UpdateChapterStatus.HideChapterStatus;
 namespace Manhwa.WebAPI.Controllers.Chapters
 {
     [ApiController]
@@ -80,6 +81,29 @@ namespace Manhwa.WebAPI.Controllers.Chapters
                 return Forbid();
             }
             var command = new PublishChapterStatusCommand
+            {
+                ChapterId = chapterId,
+                UserId = (long)userId,
+                UserRole = userRole
+            };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpPatch("chapter/{chapterId}/hidden")]
+        [Authorize]
+        public async Task<IActionResult> HiddenChapter([FromRoute] long chapterId)
+        {
+            var userId = User.GetUserId();
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+            var userRole = User.FindFirst(System.Security.Claims.ClaimTypes.Role)?.Value;
+            if (userRole == null)
+            {
+                return Forbid();
+            }
+            var command = new HideChapterStatusCommand
             {
                 ChapterId = chapterId,
                 UserId = (long)userId,
