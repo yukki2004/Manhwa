b8f7d06cb325dc4dde15dcdfd0b756b88f6f3d95
yukki2004 <dangsykien09@gmail.com>
2026-01-28 17:15:23 +0700

    feat(cunsumer) thêm consumer cho các tương tác của story

---
+++ Src/Manhwa.Application/Common/Messaging/StoryInteractionEvent.cs
@@ -0,0 +1,18 @@
+﻿using Manhwa.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manhwa.Application.Common.Messaging
+{
+    public class StoryInteractionEvent
+    {
+        public long StoryId { get; init; }
+        public long? ChapterId { get; init; }
+        public string Identity { get; init; } = string.Empty;
+        public InteractionType ActionType { get; init; }
+        public DateTimeOffset Timestamp { get; init; } = DateTimeOffset.UtcNow;
+    }
+}
---
+++ Src/Manhwa.Infrastructure/Messaging/Consumers/StoryInteractionConsumer.cs
@@ -0,0 +1,33 @@
+﻿using Manhwa.Application.Common.Interfaces;
+using Manhwa.Application.Common.Interfaces.Ranking;
+using Manhwa.Application.Common.Messaging;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manhwa.Infrastructure.Messaging.Consumers
+{
+    public class StoryInteractionConsumer : IConsumer<StoryInteractionEvent>
+    {
+        private readonly IEnumerable<IInteractionStrategy> _interactionStrategies;
+        private readonly IUnitOfWork _unitOfWork;
+        public StoryInteractionConsumer(IEnumerable<IInteractionStrategy> interactionStrategies, IUnitOfWork unitOfWork)
+        {
+            _interactionStrategies = interactionStrategies;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Consume(ConsumeContext<StoryInteractionEvent> context)
+        {
+            var message = context.Message;
+            var strategy = _interactionStrategies.FirstOrDefault(s => s.Type == message.ActionType);
+            if (strategy != null)
+            {
+                await strategy.HandleAsync(message.StoryId, message.ChapterId, message.Identity);
+            }
+        }
+
+    }
+}
